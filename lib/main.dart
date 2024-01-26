4555a834fed9e66b3dd5a756003b3d5be92d0fee
akshaykk07 <akshaykuruvangad@gmail.com>
2024-01-26 20:48:31 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home:ViewProfile()
+        home:StudentList()
       ),
       designSize: Size(390, 844),
     );
